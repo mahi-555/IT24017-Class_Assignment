14c55842334defb98f3b90c122ca75e60e6799d7
mahi-555 <moniramahin555@gmail.com>
2026-05-06 10:47:54 +0600

    Add files via upload

---
+++ HarshadNumber.java
@@ -0,0 +1,19 @@
+public class HarshadNumber {
+    public static void main(String[] args) {
+
+        int n = Integer.parseInt(args[0]); 
+
+        int sum = 0, temp = n;
+
+        while (temp > 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+
+        if (n % sum == 0) {
+            System.out.println("Harshad Number");
+        } else {
+            System.out.println("Not Harshad Number");
+        }
+    }
+}
